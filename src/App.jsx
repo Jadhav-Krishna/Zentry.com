9d455ec0619147efc2c2d4fd09a66ad4d426b119
Krishna Jadhav <krishnajadhav85017@gmail.com>
2024-11-25 05:45:46 +0530

    hero section completely done , font loading bug is fixed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Hero from './components/Hero'
+import Section from './components/Section'
 
 const App = () => {
   return (
     <main className='realtive min-h-screen w-screen overflow-x-hidden'>
       <Hero/>
+      <Section/>
     </main>
   )
 }
